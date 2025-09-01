be18d3eefaf8d041a51c9a14c64ee3135ba799cd
IAMTHEWONDERER <theoblivionitself@gmail.com>
2025-09-01 04:12:12 +0100

    sswaze

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-// main.jsx - Updated to include VisitorTracker
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppWithLoading from "./routes/AppWithLoading.jsx";
-import VisitorTracker from "./Hooks/VisitorTracker.js"; // Add this import
+import VisitorTracker from "./Hooks/VisitorTracker.js"; 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <VisitorTracker /> {/* Add visitor tracker here */}
+    <VisitorTracker /> 
     <AppWithLoading />
   </React.StrictMode>
 );
